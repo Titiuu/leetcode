119dae1759e6f5ddf158bc70cc030f5ce4dc2fa3
刘谦 <lq122561@163.com>
2022-01-16 12:17:04 +0800

    del bin

--- src/1995.cpp
+++ src/1995.cpp
@@ -1,8 +1,10 @@
 #include "head.h"
 
-class Solution {
+class Solution
+{
 public:
-    int countQuadruplets(vector<int>& nums) {
+    int countQuadruplets(vector<int> &nums)
+    {
         // vector<pair<int, int>> value_indexs(nums.size());
         // for (int i = 0; i < nums.size(); i++) {
         //     value_indexs[i] = {nums[i], i};
@@ -12,12 +14,15 @@ public:
         // });
         // sort(nums.begin(), nums.end());
         int i, j, k, l, count = 0, len = nums.size();
-        for (i = 0; i < len - 3; i++) {
-            for (j = i + 1; j < len - 2; j++) {
-                for (k = j + 1; k < len - 1; k++) {
+        for (i = 0; i < len - 3; i++)
+        {
+            for (j = i + 1; j < len - 2; j++)
+            {
+                for (k = j + 1; k < len - 1; k++)
+                {
                     for (l = k + 1; l < len; l++)
                         if (nums[i] + nums[j] + nums[k] == nums[l])
-                            count ++;
+                            count++;
                 }
             }
         }
@@ -25,7 +30,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution S;
     vector<int> n = {1, 1, 1, 3, 5, 8, 6, 3, 7};
     cout << S.countQuadruplets(n);
