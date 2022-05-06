8c2fef1fdd6937f7f2e13dc889b60f67ed07b047
刘谦 <lq122561@163.com>
2022-05-06 17:36:21 +0800

    933

---
+++ src/933.cpp
@@ -0,0 +1,15 @@
+#include "head.h"
+
+class RecentCounter {
+public:
+    queue<int> q;
+    RecentCounter() {
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        while (q.front() < t - 3000)
+            q.pop();
+        return q.size();
+    }
+};
